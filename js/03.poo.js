ac1867e54484387cded344cad8492d34c92261de
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-08-17 16:51:24 +0200

    TP3 poo
    petit commit avant refactoring de la classe Personne,
    tu a gagné, je vais devoir faire de L'ES5 juste pour terminer ce TP

--- js/03.poo.js
+++ js/03.poo.js
@@ -1,5 +1,9 @@
 console.log("03 - poo ");
 
+
+/**
+ * ça c'est de l'ES6 
+ */
 class Personne {
     nom;
     prenom;
@@ -9,7 +13,11 @@ class Personne {
         this.nom = nom;
         this.prenom = prenom;
         this.pseudo = pseudo;
-    };
+    }
+    /**
+     * 
+     * @returns une chaine de caractére formaté contenant les valeurs de propriétés de l'objet
+     */
     getNomComplet() {
         return this.nom + " " + this.prenom + " " + this.pseudo;
     }
@@ -19,14 +27,78 @@ class Personne {
 
 
 let jules = new Personne('Jules', 'LEMAIRE', 'jules77');
-let paule = new Personne("Paul", "LEMAIRE", "paul44");
+let paul = new Personne("Paul", "LEMAIRE", "paul44");
 
-console.log(jules.nom);
-console.log(jules.prenom);
-console.log(jules.pseudo);
+function afficherPersonne(personne) {
+
+    console.log(personne.nom);
+    console.log(personne.prenom);
+    console.log(personne.pseudo);
+    console.log(personne.getNomComplet());
+}
+
+afficherPersonne(jules);
+afficherPersonne(paul);
+
+jules.pseudo = "jule44";
 console.log(jules.getNomComplet());
+console.log(jules.age);
+// pour ajouter une proprièté "après coup"
+// ce qui selon moi est une ignoble erreur de programmation
+// surtout avec ECM6
+// il faut passer par prototype
+Personne.prototype.age = "NON RENSEIGNE";
+// non sérieux par pitié ne faite plus jamais ça
 
-console.log(paule.nom);
-console.log(paule.prenom);
-console.log(paule.pseudo);
-console.log(paule.getNomComplet());
+console.log(" on reteste : " + jules.age);
+
+jules.age = 30;
+console.log(" on reteste : " + jules.age);
+
+Personne.prototype.getInitiales = function () { return this.nom.charAt(0) + this.prenom.charAt(0) };
+
+console.log(jules.getInitiales());
+
+
+
+///////////////////////////////////////////////////
+// on revient à de l'ES5
+let robert = {
+    nom: "LEPREFET",
+    prenom: "Robert",
+    pseudo: "robert77",
+    getNomComplet: function () { return this.nom + " " + this.prenom + " " + this.pseudo; }
+}
+
+
+afficherPersonne(robert);
+
+/**
+ * le client hérite de personne
+ * l'héritage se faite pas un call sur la classe mére
+ * depuis le constructeur
+ * 
+ * @param {*} numeroClient 
+ * @param {*} nom 
+ * @param {*} prenom 
+ * @param {*} pseudo 
+ */
+function Client(numeroClient, nom, prenom, pseudo) {
+    // l'héritage se faite pas un call sur la classe mére
+    // depuis le constructeur
+    Personne.call(this, nom, prenom, pseudo);
+    this.numeroClient = numeroClient;
+    /**
+     * putain c'est moche
+     * @returns 
+     */
+    this.getInfo = () => {
+        return this.numeroClient + "  " + this.nom + " " + this.prenom;
+    }
+
+}
+
+steve = new Client("A01", "LUCAS", "Steve", "steve44");
+afficherPersonne(steve);
+console.log(steve.numeroClient);
+console.log(steve.getInfo());
